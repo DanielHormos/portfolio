b4c95c8d99876d242d73350c12a26da6e7165d7f
Daniel Hormos <daniel_danne32@hotmail.com>
2024-11-18 00:04:06 +0100

    Fixed tailwind

--- src/features/profile/ProfilePicture.jsx
+++ src/features/profile/ProfilePicture.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export function ProfilePicture() {
   return (
     <Image
-      className="rounded-full"
+      className="rounded-full mb-3"
       width={250}
       height={0}
       src={"/120799414.jfif"}
--- src/features/profile/profile.jsx
+++ src/features/profile/profile.jsx
@@ -1,9 +1,11 @@
+import { ProfileInfo } from "./ProfileInfo";
 import { ProfilePicture } from "./ProfilePicture";
 
 export function Profile() {
   return (
-    <>
+    <div className="p-10 border border-l-rose-300">
       <ProfilePicture />
-    </>
+      <ProfileInfo />
+    </div>
   );
 }
