507bb0cd67f061ee50a5e5685983773ce61f2a77
Daniel Hormos <daniel_danne32@hotmail.com>
2024-12-10 20:54:34 +0100

    removed background color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,9 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${roboto.className} antialiased bg-customBlack`}>
-        {children}
-      </body>
+      <body className={`${roboto.className} antialiased`}>{children}</body>
     </html>
   );
 }
